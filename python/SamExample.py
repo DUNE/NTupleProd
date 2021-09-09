c9e9b276acb6048eefcf3f3400a73f5328255fc1
Heidi Schellman <Heidi.Schellman@oregonstate.edu>
2021-09-09 12:42:39 -0700

    debug

--- python/SamExample.py
+++ python/SamExample.py
@@ -70,7 +70,7 @@ def samExample(def_name,larargs):
 
     #try:
     #cpid = samweb.startProcess(projecturl, appFamily, appName, appVersion, deliveryLocation, node=node, description=process_description, maxFiles=maxFiles, schemas=schemas)
-    consumer_id = samweb.startProcess(projecturl=project_uri, appFamily=opts["appFamily"], appName=opts["appName"], appVersion=opts["appVersion"], node=socket.gethostname(), description=opts["process_description"], maxFiles=opts["MaxFiles"], schemas="root")
+    consumer_id = samweb.startProcess(project_uri, opts["appFamily"], opts["appName"], opts["appVersion"], node=socket.gethostname(), description=opts["process_description"], maxFiles=opts["MaxFiles"], schemas="root")
       #consumer_id = ifdh_handle.establishProcess(project_uri,"ana",os.getenv("DUNE_RELEASE"), socket.gethostname(),os.getenv("GRID_USER"),"root-tuple")
     print (mytime(),"Got SAM consumer id:",consumer_id)
     #except Exception:
