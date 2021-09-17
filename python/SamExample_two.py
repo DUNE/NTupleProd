d117b7835f48d48ae20994669d3243651c95102e
Heidi Schellman <heidi.schellman@oregonstate.edu>
2021-09-16 17:35:21 -0700

    make a version of example that merges input metadata

---
+++ python/SamExample_two.py
@@ -0,0 +1,173 @@
+# script to do short tests of sam projects interactively
+
+import os,sys,time,string,datetime,socket
+import samweb_client
+import json
+import ifdh
+import subprocess
+import argparse
+from mergeMeta import *
+
+parser = argparse.ArgumentParser(description='Test Sam Project')
+parser.add_argument("-c", type=str, help="Name of fcl file", default="test.fcl")
+args = parser.parse_args()
+
+ 
+# test setup to be fixed up later with real args
+e = "unknown"
+statuscodes ={}
+opts = {}
+opts["fcl"]="test.fcl"
+opts["n_consumers"]=2
+opts["appFamily"] = "protoduneana"
+opts["appName"]= "pdspana"
+opts["appVersion"]=os.getenv("PROTODUNEANA_VERSION")
+opts["process_description"]="testing sam access"
+opts["MaxFiles"]=2
+opts["jsonName"]="ana_hist.root.json"
+opts["rootName"]="pduneana.root"
+opts["runType"]="protodune-sp"
+opts["dataTier"]="storage-testing"
+opts["dataStream"]="physics"
+
+# need this
+samweb = samweb_client.SAMWebClient(experiment='dune')
+
+def mytime():
+  return datetime.datetime.now().strftime("%Y-%m-%d-%H%M.%S")
+
+def test():
+  
+  #larargs = ["-c./test.fcl"]+["-n100"]
+  larargs = ["-c" + args.c]+["-n20"]
+  project_name = "PDSPProd4a_MC_1GeV_reco1_sce_datadriven_v1"
+  samExample(project_name,larargs)
+  
+def process_sam(project_url,project_name,consumer_id,larargs):
+  
+  larcommand = ["lar"]+larargs
+  #larcommand += ["--sam-inherit-run-type"]
+  larcommand += ["--sam-web-uri=%s"%project_url]
+  larcommand += ["--sam-process-id=%s"%consumer_id]
+  larcommand += ["--sam-application-family=%s"%opts["appFamily"]]
+  larcommand += ["--sam-application-version=%s"%opts["appVersion"]]
+  #arcommand += ["--sam-application-name=pdspana"]
+  filename = "protoduneana_%s_%s"%(project_name,consumer_id)
+  rootname = filename+".root"
+  logfile = open(filename+".out",'w')
+  errfile = open(filename+".err",'w')
+  jsonname =filename+"_temp.json"
+  print ("SamExample:",mytime(),"try to launch",larcommand,consumer_id)
+  start_time = timeform(datetime.datetime.now())
+  ret = subprocess.run(larcommand,stdout=logfile,stderr=errfile)
+  end_time = timeform(datetime.datetime.now())
+  status = ret.returncode
+  print ("lar returned:",status)
+  # now make better metadata
+  mopts = {}
+  maker = mergeMeta(mopts)
+  maker.source = "samweb"
+  if status == 0 and os.path.exists(opts["jsonName"]):
+    print ("found ",opts["jsonName"])
+    os.rename(opts["jsonName"],jsonname)
+    os.rename(opts["rootName"],rootname)
+    json_file = open(jsonname,'r')
+    fixed = open(jsonname.replace("_temp","_fixed"),'w')
+    if not json_file:
+        raise IOError('Unable to open json file %s.' % jsonname)
+    else:
+      md = json.load(json_file)
+      json_file.close()
+      externals = {}
+      #set things we need to make certain don't inherit from parents
+      externals["file_name"]=rootname
+      externals["application"]={"family": opts["appFamily"],"name": opts["appName"],"version": opts["appVersion"]}
+      externals["data_tier"]=opts["dataTier"]
+      externals["file_size"]=os.path.getsize(rootname)
+      externals["data_stream"]=opts["dataStream"]
+      externals["start_time"]=start_time
+      externals["end_time"]=end_time
+      # identify the parents so you can merge
+      parents = md["parents"]
+      plist = []
+      for p in parents:
+        plist += [(p["file_name"])]
+      status = maker.checkmerge(plist)
+      if status:
+        newmd = maker.concatenate(plist,externals)
+      # patch the runs with the runtype as the art option doesn't work
+#      runs = md["runs"]
+#      newruns = []
+#      for run in runs:
+#        run[2] = opts["runType"]
+#        newruns.append(run)
+#      print ("run fix",newruns)
+#      md["runs"]=newruns
+      else:
+        print (" could not merge the inputs, sorry")
+        return 1
+      json.dump(newmd,fixed, indent=2,separators=(',',': '))
+      
+  else:
+    print ("no json or failure",status)
+    return 1
+  print ("subprocess returns:", status)
+  return 0
+  
+def startProject(def_name):
+  project_name = def_name+"_"+mytime()
+  val = samweb.startProject(project_name,def_name)
+  print ("startProject",val)
+  return val
+
+
+def samExample(def_name,larargs):
+
+  print ("SamExample:",mytime(), "Getting files from Sam definition with name:",def_name)
+  
+  info = startProject(def_name)
+  
+  project_name = info["project"]
+  project_url = info["projectURL"]
+
+  print ("Got SAM project url:",project_url)
+ 
+  consumer_id = 0
+  
+  n_consumers = opts["n_consumers"]
+
+  for nc in range(0,n_consumers):  # do several consumers, each with MaxFiles files
+  
+    
+    consumer_id = samweb.startProcess(project_url, opts["appFamily"], opts["appName"], opts["appVersion"], node=socket.gethostname(), description=opts["process_description"], maxFiles=opts["MaxFiles"], schemas="root")
+    
+    process_url = samweb.makeProcessUrl(project_url, consumer_id)
+    print ("SamExample:",mytime(),"Got SAM consumer id:",consumer_id, process_url)
+    consumerok = True
+    
+    # do the actual processing
+    
+    status = process_sam(project_url,project_name,consumer_id,larargs)
+    
+    consumerok = (status==0)
+    
+    print (" larsoft returned status", status)
+ 
+    #if we got this far then we trust the project so mark it as completed
+    try:
+        if consumerok:
+          print ("SamExample:",mytime(), " set consumer ", consumer_id, "ok")
+          samweb.stopProcess(process_url )
+         
+        else:
+          print ("SamExample:",mytime(), " set consumer ", consumer_id, "bad")
+          samweb.setProcessStatus('bad',process_url )
+         
+    except Exception:
+        print ("SamExample:",mytime()," can't even set to bad as consumer status failed",e)
+      
+  samweb.stopProject(project_url)
+  out = samweb.projectSummaryText(project_url)
+  print (out)
+
+test()
