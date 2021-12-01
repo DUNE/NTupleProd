36f907b2de79df5e941caffe5f8771c6c78a0e6e
Jacob Calcutt <calcuttj@msu.edu>
2021-12-01 09:14:13 -0600

    audit.py: Added a flag to check if any file has been added to a requested new dataset

--- python/audit.py
+++ python/audit.py
@@ -10,6 +10,8 @@ parser.add_argument('-d', '--dataset', type=str, help='Dataset to check',
 parser.add_argument('-v', '--version', type=str, help='protoduneana version',
                     default=None)
 parser.add_argument('-u', '--user', type=str, help='User', default=None)
+parser.add_argument('--new-dataset', type=str,
+                    help='Which dataset was this added to?', default=None)
 
 make_note = "Create a definition with the supplied name."
 make_note += "Note: Name must contain 'makeup'"
@@ -29,6 +31,8 @@ samweb = samweb_client.SAMWebClient(experiment='dune')
 not_parent = "data_tier root-tuple-virtual"
 if args.user:
   not_parent += " and user %s"%args.user
+if args.new_dataset:
+  not_parent += ' and defname:%s'%args.new_dataset
 
 query = "defname:%s and not isparentof:(%s)"%(args.dataset, not_parent)
 
@@ -40,6 +44,7 @@ print("Got %i files"%len(files))
 
 if args.make:
   print("Supplied definition %s to --make"%args.make)
+  do_exit = False
 
   try:
     a = samweb.descDefinition(args.make)
